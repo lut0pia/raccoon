ef752ef21b262336b56cd416377db39a16afb7bf
Lucien Catonnet <lutopia@hotmail.fr>
2019-01-18 00:06:25 +0100

    Added vm_worker
    And a few basic messages, namely code to put code in the VM, update to trigger an update, and blit to trigger a rendering client-side

--- src/js/vm.js
+++ src/js/vm.js
@@ -1,7 +1,29 @@
-function rcn_vm() {
+// Raccoon virtual machine
+// This is the client that spawns the web worker
 
+function rcn_vm() {
+  this.worker = new Worker(rcn_vm_worker_url);
+  var vm = this;
+  this.worker.onmessage = function(e) { vm.onmessage(e); }
 }
 
 rcn_vm.prototype.load_paw = function(paw) {
   this.ram = paw.rom.slice();
+  this.worker = new Worker(rcn_vm_worker_url);
+  this.load_code(paw.code);
+}
+
+rcn_vm.prototype.load_code = function(code) {
+  this.worker.postMessage({type:'code',code:code});
+}
+
+rcn_vm.prototype.update = function() {
+  this.worker.postMessage({type:'update'});
+}
+
+rcn_vm.prototype.onmessage = function(e) {
+  console.log(e);
+  switch(e.data.type) {
+    case 'blit': this.canvas.blit(e.data.x, e.data.y, e.data.w, e.data.h, e.data.pixels, e.data.palette); break;
+  }
 }
---
+++ src/js/vm_worker.js
@@ -0,0 +1,59 @@
+// Raccoon virtual machine
+// This is the worker script that gets executed inside a web worker
+
+function rcn_vm_worker_function() {
+  const rcn_vm_ram_size = 32 * 1024; // = 32KiB
+  const rcn_vm_ram_palette_offset = 0x4000;
+  const rcn_vm_ram_screen_offset = 0x6000;
+
+  // Keep parts of the API local
+  var _Function = Function;
+  var _postMessage = postMessage;
+
+  // Remove parts of the API
+  delete eval;
+  delete Function;
+  delete postMessage;
+  delete Worker;
+  delete XMLHttpRequest;
+
+  var ram = new Uint8Array(rcn_vm_ram_size);
+
+  // Implement raccoon API
+  pset = function(x, y, p) {
+    var pixel_index = rcn_vm_ram_screen_offset+y*128+(x>>1);
+    var pixel = ram[pixel_index];
+    if((x % 2) == 0) {
+      pixel &= 0xf0;
+      pixel |= p;
+    } else {
+      pixel &= 0xf;
+      pixel |= p<<4;
+    }
+    ram[pixel_index] = pixel;
+  }
+  palset = function(i, r, g, b) {
+    ram[rcn_vm_ram_palette_offset+i*3+0] = r;
+    ram[rcn_vm_ram_palette_offset+i*3+1] = g;
+    ram[rcn_vm_ram_palette_offset+i*3+2] = b;
+  }
+
+  onmessage = function(e) {
+    switch(e.data.type) {
+      case 'code':
+        // TODO: allow function thing() {} syntax to work as expected
+        (new _Function(e.data.code))();
+        break;
+      case 'update':
+        update(); // This is user-defined
+        _postMessage({
+          type:'blit', x:0, y:0, w:128, h:128,
+          pixels:ram.slice(rcn_vm_ram_screen_offset, rcn_vm_ram_screen_offset+0x2000),
+          palette:ram.slice(rcn_vm_ram_palette_offset, rcn_vm_ram_palette_offset+0x30),
+        });
+        break;
+    }
+  }
+}
+
+const rcn_vm_worker_url = URL.createObjectURL(new Blob(['('+rcn_vm_worker_function.toString()+')()'], {type: 'text/javascript'}));
