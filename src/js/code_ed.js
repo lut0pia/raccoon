ef8b2882422bae919b452b779b69d0ac994d3722
Lucien Catonnet <lutopia@hotmail.fr>
2019-01-19 16:35:44 +0100

    Added code_ed window draft

---
+++ src/js/code_ed.js
@@ -0,0 +1,17 @@
+// Raccoon code editor
+
+function rcn_code_ed() {
+  this.window = new rcn_window('code_ed');
+  this.textarea = document.createElement('textarea');
+  this.window.add_child(this.textarea);
+
+  this.run_button = document.createElement('input');
+  this.run_button.type = 'button';
+  this.run_button.value = 'Run';
+  this.window.add_child(this.run_button);
+
+  var code_ed = this;
+  this.run_button.onclick = function() {
+    rcn_global_vm.load_code(code_ed.textarea.value);
+  }
+}
