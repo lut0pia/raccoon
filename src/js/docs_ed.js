066d55109658d44df852ec16763e5bd5623e2db1
Lucien Catonnet <lutopia@hotmail.fr>
2019-01-29 22:21:24 +0100

    Use different src in documentation viewer's iframe

--- src/js/docs_ed.js
+++ src/js/docs_ed.js
@@ -3,6 +3,6 @@
 function rcn_docs_ed() {
   this.window = new rcn_window('docs_ed', 'Documentation Viewer');
   this.iframe = document.createElement('iframe');
-  this.iframe.src = 'docs';
+  this.iframe.src = location.protocol=='file:' ? 'docs/README.md' : location.href + '/docs/';
   this.window.add_child(this.iframe);
 }
