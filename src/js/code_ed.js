955f600a7ba25c7f8089e3157df067728633a963
Lucien Catonnet <lutopia@hotmail.fr>
2019-02-01 22:00:04 +0100

    Improved Tab support + Return at same column

--- src/js/code_ed.js
+++ src/js/code_ed.js
@@ -3,21 +3,57 @@
 function rcn_code_ed() {
   this.window = new rcn_window('code_ed', 'Code Editor');
   this.textarea = document.createElement('textarea');
-  this.textarea.onkeydown = rcn_code_ed_textarea_onkeydown;
+
+  var code_ed = this;
+  this.textarea.onkeydown = function(e) {
+    const key_code = e.keyCode || e.which;
+    const tab_size = 2;
+
+    const start = this.selectionStart;
+    const end = this.selectionEnd;
+    const start_line_beg = this.value.lastIndexOf('\n', start-1)+1;
+    const start_line_end = (this.value.indexOf('\n', start) + 1 || this.value.length) - 1;
+    const end_line_beg = this.value.lastIndexOf('\n', end-1)+1;
+    const line_last_space = Math.max(0, this.value.substr(start_line_beg).search(/[^ ]/))+start_line_beg;
+
+    if(key_code == 13) { // Enter key
+      if(e.ctrlKey) { // CTRL + Enter
+        code_ed.apply();
+      } else if(!e.shiftKey) { // Simple Enter
+        e.preventDefault();
+        rcn_code_ed_textarea_insert_text(this, '\n'+' '.repeat(line_last_space-start_line_beg));
+      }
+    } else if (key_code == 9) { // Tab key
+      e.preventDefault();
+
+      if(start_line_beg == end_line_beg) { // Same line selection
+        if(e.shiftKey) {
+          this.selectionEnd = line_last_space;
+          this.selectionStart = Math.max(line_last_space - tab_size, start_line_beg);
+          var diff = this.selectionStart - this.selectionEnd;
+          rcn_code_ed_textarea_insert_text(this, '');
+          this.selectionStart = Math.max(start_line_beg, start + diff);
+          this.selectionEnd = Math.max(start_line_beg, end + diff);
+        } else {
+          const chars_offset = tab_size - ((start-start_line_beg) % tab_size);
+          rcn_code_ed_textarea_insert_text(this, ' '.repeat(chars_offset));
+        }
+      }
+    }
+  };
   this.textarea.onchange = function() {
     rcn_global_bin.code = this.value;
   };
   this.window.add_child(this.textarea);
 
-  this.apply_button = document.createElement('input');
-  this.apply_button.type = 'button';
-  this.apply_button.value = 'Apply';
-  this.window.add_child(this.apply_button);
+  this.apply_button = rcn_ui_button({
+    window: this.window,
+    value:'Apply',
+    onclick: function() {
+      code_ed.apply();
+    },
+  });
 
-  var code_ed = this;
-  this.apply_button.onclick = function() {
-    rcn_global_vm.load_code(code_ed.textarea.value);
-  }
   this.window.addEventListener('rcnbinchange', function(e) {
     if(e.detail.code) {
       code_ed.textarea.value = rcn_global_bin.code;
@@ -25,24 +61,8 @@ function rcn_code_ed() {
   });
 }
 
-function rcn_code_ed_textarea_onkeydown(e) {
-  const key_code = e.keyCode || e.which;
-  const tab_size = 2;
-
-  if (key_code == 9) { // Tab key
-    e.preventDefault();
-
-    const start = this.selectionStart;
-    const end = this.selectionEnd;
-    const start_line_beg = this.value.lastIndexOf('\n', start-1)+1;
-    const end_line_beg = this.value.lastIndexOf('\n', end-1)+1;
-
-    const chars_offset = tab_size - ((start-start_line_beg) % tab_size);
-
-    if(start_line_beg == end_line_beg) { // Same line selection
-      rcn_code_ed_textarea_insert_text(this, ' '.repeat(chars_offset));
-    }
-  }
+rcn_code_ed.prototype.apply = function() {
+  rcn_global_vm.load_code(this.textarea.value);
 }
 
 function rcn_code_ed_textarea_insert_text(textarea, text) {
