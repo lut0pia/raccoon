ec70b31c9577e621c33eaf685b810ed0f85ad92a
Lucien Catonnet <lutopia@hotmail.fr>
2019-02-05 10:28:09 +0100

    Added autoapply mechanism

--- src/js/vm_ed.js
+++ src/js/vm_ed.js
@@ -7,15 +7,28 @@ function rcn_vm_ed() {
   this.vm = rcn_global_vm = new rcn_vm();
   this.add_child(this.vm.canvas.node);
 
+  var vm_ed = this;
+
   // Create reboot button
   this.reboot_button = document.createElement('input');
   this.reboot_button.type = 'button';
   this.reboot_button.value = 'Reboot';
-  this.reboot_button.vm_ed = this;
   this.reboot_button.onclick = function() {
-    this.vm_ed.reboot();
+    vm_ed.reboot();
   }
   this.add_child(this.reboot_button);
+
+  // Create autoapply checkbox
+  var autoapply_checkbox_id = 'autoapply_checkbox'; // TODO: this will have to depend on window id
+  this.autoapply_checkbox = document.createElement('input');
+  this.autoapply_checkbox.id = autoapply_checkbox_id;
+  this.autoapply_checkbox.type = 'checkbox';
+  this.autoapply_checkbox.checked = true;
+  this.add_child(this.autoapply_checkbox);
+  this.autoapply_checkbox_label = document.createElement('label');
+  this.autoapply_checkbox_label.htmlFor = autoapply_checkbox_id;
+  this.autoapply_checkbox_label.innerText = 'Autoapply';
+  this.add_child(this.autoapply_checkbox_label);
 
   // Create error log
   this.error_log = document.createElement('div');
@@ -28,10 +41,13 @@ function rcn_vm_ed() {
   }
   this.add_child(this.error_log);
 
-  var vm_ed = this;
   this.addEventListener('rcnbinchange', function(e) {
     if(e.detail.load) {
+      // We just loaded a new bin, therefore we reboot
       vm_ed.reboot();
+    } else if(vm_ed.autoapply_checkbox.checked) {
+      // If autoapply is on, we directly load changed rom into ram
+      vm_ed.vm.load_memory(rcn_global_bin.rom.slice(e.detail.begin, e.detail.end), e.detail.begin);
     }
   });
 }
