3a5b3c9c1d7603732d0eb8d0239bfd54f926ab58
Lucien Catonnet <lutopia@hotmail.fr>
2019-02-06 21:33:10 +0100

    Fixed iframe recursion hell

--- src/js/docs_ed.js
+++ src/js/docs_ed.js
@@ -4,6 +4,6 @@ function rcn_docs_ed() {
   this.__proto__.__proto__ = rcn_window.prototype;
   rcn_window.call(this, 'docs_ed', 'Documentation Viewer');
   this.iframe = document.createElement('iframe');
-  this.iframe.src = location.protocol=='file:' ? 'docs/README.md' : location.href + '/docs/';
+  this.iframe.src = location.protocol=='file:' ? 'docs/README.md' : location.origin + '/docs/';
   this.add_child(this.iframe);
 }
