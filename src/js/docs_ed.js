cba4d3b8ad80ce3d5eedc69021e60bb13012ae8a
Lucien Catonnet <lutopia@hotmail.fr>
2019-02-13 22:13:03 +0100

    Added rcndoclookup event

--- src/js/docs_ed.js
+++ src/js/docs_ed.js
@@ -6,4 +6,16 @@ function rcn_docs_ed() {
   this.iframe = document.createElement('iframe');
   this.iframe.src = location.protocol=='file:' ? 'docs/README.md' : location.origin + '/docs/';
   this.add_child(this.iframe);
+
+  var docs_ed = this;
+  if(location.protocol == 'file:') {
+    this.addEventListener('rcndoclookup', function(e) {
+      docs_ed.lookup(e.detail.key);
+    });
+  }
+}
+
+rcn_docs_ed.prototype.lookup = function(key) {
+  key = rcn_doc_lookup[key] || key;
+  this.iframe.src = location.origin + '/docs/#' + rcn_doc_lookup[key];
 }
