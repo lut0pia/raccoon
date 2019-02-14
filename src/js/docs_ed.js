dc54d49ba00fd5ab9df05b8b039cc1440c71e764
Lucien Catonnet <lutopia@hotmail.fr>
2019-02-14 13:52:10 +0100

    rcndoclookup isn't supported when local browsing

--- src/js/docs_ed.js
+++ src/js/docs_ed.js
@@ -8,7 +8,7 @@ function rcn_docs_ed() {
   this.add_child(this.iframe);
 
   var docs_ed = this;
-  if(location.protocol == 'file:') {
+  if(location.protocol != 'file:') {
     this.addEventListener('rcndoclookup', function(e) {
       docs_ed.lookup(e.detail.key);
     });
