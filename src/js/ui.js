165e4cc09b3653619f738225e4e1d97b01a7cdbc
Lucien Catonnet <lutopia@hotmail.fr>
2019-04-15 21:40:49 +0200

    Added rcn_ui_select helper function

--- src/js/ui.js
+++ src/js/ui.js
@@ -9,3 +9,19 @@ function rcn_ui_button(o) {
   }
   return button;
 }
+
+function rcn_ui_select(o) {
+  let select = document.createElement('select');
+  for(let i in o.options) {
+    if(o.options.hasOwnProperty(i)) {
+      let option = document.createElement('option');
+      option.innerText = o.options[i];
+      option.value = i;
+      select.appendChild(option);
+    }
+  }
+  if(o.onchange) {
+    select.onchange = o.onchange;
+  }
+  return select;
+}
