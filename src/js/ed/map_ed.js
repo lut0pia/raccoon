7aace7e164696d3fade27867ac8909d1e8bd3e93
Lucien Catonnet <lutopia@hotmail.fr>
2019-07-21 21:42:53 +0200

    Added more hover feedback to map editor

--- src/js/ed/map_ed.js
+++ src/js/ed/map_ed.js
@@ -8,6 +8,8 @@ function rcn_map_ed() {
   // Init map editing state
   this.current_offset_x = 0;
   this.current_offset_y = 0;
+  this.current_hover_x = null;
+  this.current_hover_y = null;
 
   const map_ed = this;
 
@@ -46,12 +48,31 @@ function rcn_map_ed() {
         map_ed.current_offset_y = shift_start_offset_y + (shift_start_client_y - e.clientY) / (vp.mul * 8);
         map_ed.current_offset_x = Math.max(0, Math.min(128 - 16, map_ed.current_offset_x)) << 0;
         map_ed.current_offset_y = Math.max(0, Math.min(64 - 16, map_ed.current_offset_y)) << 0;
-        map_ed.update_map_coords_text();
         map_ed.update_map_canvas();
         e.preventDefault();
       }
     }
   });
+  this.map_canvas.node.addEventListener('mousemove', function(e) {
+    const canvas_coords = this.getBoundingClientRect();
+    const tex_coords = map_ed.map_canvas.client_to_texture_coords(e.clientX - canvas_coords.x, e.clientY - canvas_coords.y);
+    map_ed.update_hovering(tex_coords);
+  });
+  this.map_canvas.onpostflush = function() {
+    if(map_ed.current_hover_x === null) {
+      return;
+    }
+    // Draw hover outline
+    const vp = this.compute_viewport();
+    const x = vp.x + map_ed.current_hover_x * vp.mul * 8;
+    const y = vp.y + map_ed.current_hover_y * vp.mul * 8;
+    const w = 1 * vp.mul * 8;
+    const h = 1 * vp.mul * 8;
+    this.draw_quad(x - 2, y - 2, 2, h + 4, 1, 1, 1, 1);
+    this.draw_quad(x + w, y - 2, 2, h + 4, 1, 1, 1, 1);
+    this.draw_quad(x, y - 2, w, 2, 1, 1, 1, 1);
+    this.draw_quad(x, y + h, w, 2, 1, 1, 1, 1);
+  };
   this.map_wrapper.appendChild(this.map_canvas.node);
 
   // Create apply button
@@ -90,7 +111,11 @@ function rcn_map_ed() {
     map_ed.map_canvas.flush();
   });
 
-  this.update_map_coords_text();
+  this.addEventListener('rcn_current_sprite_change', function() {
+    map_ed.update_map_canvas();
+  });
+
+  this.update_hovering();
   this.update_map_canvas();
 }
 
@@ -119,10 +144,28 @@ rcn_map_ed.prototype.get_tile = function(map_x, map_y) {
   return rcn_global_bin.rom[tile_index];
 }
 
-rcn_map_ed.prototype.update_map_coords_text = function() {
-  this.map_coords_text.innerText =
-    this.current_offset_x.toString().padStart(3, '0') + ';' +
-    this.current_offset_y.toString().padStart(3, '0');
+rcn_map_ed.prototype.update_hovering = function(tex_coords) {
+  if(tex_coords) {
+    const rel_tile_x = tex_coords.x >> 3;
+    const rel_tile_y = tex_coords.y >> 3;
+    const abs_tile_x = rel_tile_x + this.current_offset_x;
+    const abs_tile_y = rel_tile_y + this.current_offset_y;
+
+    this.map_coords_text.innerText =
+    abs_tile_x.toString().padStart(3, '0') + ';' +
+    abs_tile_y.toString().padStart(3, '0');
+
+    if(rel_tile_x !== this.current_hover_x || rel_tile_y !== this.current_hover_y) {
+      this.current_hover_x = rel_tile_x;
+      this.current_hover_y = rel_tile_y;
+      this.update_map_canvas();
+    }
+  } else if(this.current_hover_x !== null) {
+    this.current_hover_x = null;
+    this.current_hover_y = null;
+    this.map_coords_text.innerText = '???;???';
+    this.update_map_canvas();
+  }
 }
 
 rcn_map_ed.prototype.update_map_canvas = function() {
@@ -131,20 +174,28 @@ rcn_map_ed.prototype.update_map_canvas = function() {
   const pixels = new Uint8Array(((map_w * map_h) << 6) >> 1);
   const map_row_size = (map_w << 3) >> 1;
 
+  const draw_tile = function(pixels, mx, my, spr) {
+    const pix_x = mx << 3;
+    const pix_y = my << 3;
+    const pix_index = (pix_y<<6)+(pix_x>>1);
+    const spr_tex_index = ((spr & 0xf) << 2) + ((spr >> 4) << 9);
+    const spr_row_size = 4;
+
+    for(let i = 0; i < 8; i++) {
+      const spr_row_index = spr_tex_index + (i << 6);
+      pixels.set(rcn_global_bin.rom.slice(spr_row_index, spr_row_index + spr_row_size), pix_index + i * map_row_size);
+    }
+  }
+
   for(let mx = 0; mx < map_w; mx++) {
     for(let my = 0; my < map_h; my++) {
-      const pix_x = mx << 3;
-      const pix_y = my << 3;
-      const pix_index = (pix_y<<6)+(pix_x>>1);
-      const spr = this.get_tile(mx, my);
-      const spr_tex_index = ((spr & 0xf) << 2) + ((spr >> 4) << 9);
-      const spr_row_size = 4;
-
-      for(let i = 0; i < 8; i++) {
-        const spr_row_index = spr_tex_index + (i << 6);
-        pixels.set(rcn_global_bin.rom.slice(spr_row_index, spr_row_index + spr_row_size), pix_index + i * map_row_size);
-      }
+      draw_tile(pixels, mx, my, this.get_tile(mx, my));
     }
+  }
+
+  // Draw hovered tile with stamp
+  if(this.current_hover_x !== null) {
+    draw_tile(pixels, this.current_hover_x, this.current_hover_y, rcn_current_sprite);
   }
 
   this.map_canvas.set_size(map_w << 3, map_h << 3);
