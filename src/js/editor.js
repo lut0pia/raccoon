d73b1e09d833c9b3f4db257988a4986b2005412f
Lucien Catonnet <lutopia@hotmail.fr>
2020-06-14 15:58:56 +0200

    Moved rcn_download_file to editor.js

--- src/js/editor.js
+++ src/js/editor.js
@@ -979,3 +979,13 @@ async function rcn_upload_file() {
     file_input.click();
   });
 }
+
+function rcn_download_file(p) {
+  const e = document.createElement('a');
+  e.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(p.content));
+  e.setAttribute('download', p.file_name);
+  e.style.display = 'none';
+  document.body.appendChild(e);
+  e.click();
+  document.body.removeChild(e);
+}
