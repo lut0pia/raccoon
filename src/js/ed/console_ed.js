8b78c92ca9fc25310f2523f11a706f8db1b6e93c
Lucien Catonnet <lutopia@hotmail.fr>
2021-01-04 23:38:29 +0100

    console_ed: Scroll to code editor line when clicking on callstack

--- src/js/ed/console_ed.js
+++ src/js/ed/console_ed.js
@@ -18,7 +18,7 @@ function rcn_console_ed() {
   });
 
   this.addEventListener('rcn_reboot', function() {
-    console_ed.log('Rebooting...');
+    console_ed.log_text('Rebooting...');
   });
 }
 
@@ -27,28 +27,43 @@ rcn_console_ed.prototype.type = 'console_ed';
 rcn_console_ed.prototype.group = 'code';
 rcn_console_ed.prototype.docs_link = 'console';
 
-rcn_console_ed.prototype.log = function(text) {
+rcn_console_ed.prototype.log_element = function(element) {
   const at_end = this.log_container.scrollTop >= this.log_container.scrollHeight - this.log_container.clientHeight;
-  const log_el = document.createElement('article');
-  log_el.innerText = text;
-  this.log_container.appendChild(log_el);
+  this.log_container.appendChild(element);
   if(at_end) {
     this.log_container.scrollTop = this.log_container.scrollHeight;
   }
+}
+
+rcn_console_ed.prototype.log_text = function(text) {
+  const log_el = document.createElement('article');
+  log_el.innerText = text;
+  this.log_element(log_el);
   return log_el;
 }
 
 rcn_console_ed.prototype.log_error = function(error) {
-  const text = `Error: ${error.message}
-    ${error.stack.map(l => `${l.func}:${l.line}`).join('\n')}`;
-  const log_el = this.log(text);
+  const log_el = document.createElement('article');
+  log_el.innerHTML = `Error: ${error.message}`;
   log_el.classList.add('error');
+  log_el.append(...error.stack.map(l => {
+    const line_el = document.createElement('line');
+    line_el.innerHTML = `> ${l.func}:${l.line}`;
+    line_el.addEventListener('click', () => {
+      const code_ed = rcn_find_editor(rcn_code_ed);
+      if(code_ed) {
+        code_ed.scroll_to_line(l.line);
+      }
+    });
+    return line_el;
+  }));
+  this.log_element(log_el);
 }
 
 rcn_console_ed.prototype.execute = function() {
   const vm_ed = rcn_find_editor(rcn_vm_ed);
   if(vm_ed && vm_ed.vm.worker) {
-    this.log(`Execute: ${this.input.value}`);
+    this.log_text(`Execute: ${this.input.value}`);
     vm_ed.vm.load_code(this.input.value);
     this.input.value = '';
   }
